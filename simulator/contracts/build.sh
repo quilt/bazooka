ffcda74c1f8caf421f18e6081d3b4d08f72770bc
Matt Garnett <matt.garnett@me.com>
2020-05-06 16:55:44 -0600

    contract => contracts module name

--- simulator/contracts/build.sh
+++ simulator/contracts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-abigen --abi build/deployer.abi --bin build/deployer.bin --pkg contract --type Deployer --out deployer.go
+abigen --abi build/deployer.abi --bin build/deployer.bin --pkg contracts --type Deployer --out deployer.go
