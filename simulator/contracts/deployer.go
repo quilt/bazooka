2870200925d3708e9758f38265fa60f05dce14f8
Matt Garnett <matt.garnett@me.com>
2020-05-06 16:10:38 -0600

    add deployer contract

---
+++ simulator/contracts/deployer.go
@@ -0,0 +1,211 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package contract
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = abi.U256
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// DeployerABI is the input ABI used to generate the binding from.
+const DeployerABI = "[{\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"code\",\"type\":\"bytes\"},{\"internalType\":\"bytes32\",\"name\":\"salt\",\"type\":\"bytes32\"}],\"name\":\"deploy\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"addr\",\"type\":\"address\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+
+// DeployerBin is the compiled bytecode used for deploying new contracts.
+var DeployerBin = "0x608060405234801561001057600080fd5b50610149806100206000396000f3fe608060405234801561001057600080fd5b506004361061002b5760003560e01c80634af63f0214610030575b600080fd5b6100d86004803603604081101561004657600080fd5b81019060208101813564010000000081111561006157600080fd5b82018360208201111561007357600080fd5b8035906020019184600183028401116401000000008311171561009557600080fd5b91908080601f01602080910402602001604051908101604052809392919081815260200183838082843760009201919091525092955050913592506100f4915050565b604080516001600160a01b039092168252519081900360200190f35b6000818351602085016000f59050803b61010d57600080fd5b9291505056fea26469706673582212200edc061df51a188fdfcd658a4275dddb39c5b578d17ab962f555d09b7ed8405b64736f6c63430006060033"
+
+// DeployDeployer deploys a new Ethereum contract, binding an instance of Deployer to it.
+func DeployDeployer(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Deployer, error) {
+	parsed, err := abi.JSON(strings.NewReader(DeployerABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(DeployerBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Deployer{DeployerCaller: DeployerCaller{contract: contract}, DeployerTransactor: DeployerTransactor{contract: contract}, DeployerFilterer: DeployerFilterer{contract: contract}}, nil
+}
+
+// Deployer is an auto generated Go binding around an Ethereum contract.
+type Deployer struct {
+	DeployerCaller     // Read-only binding to the contract
+	DeployerTransactor // Write-only binding to the contract
+	DeployerFilterer   // Log filterer for contract events
+}
+
+// DeployerCaller is an auto generated read-only Go binding around an Ethereum contract.
+type DeployerCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// DeployerTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type DeployerTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// DeployerFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type DeployerFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// DeployerSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type DeployerSession struct {
+	Contract     *Deployer         // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// DeployerCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type DeployerCallerSession struct {
+	Contract *DeployerCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts   // Call options to use throughout this session
+}
+
+// DeployerTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type DeployerTransactorSession struct {
+	Contract     *DeployerTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts   // Transaction auth options to use throughout this session
+}
+
+// DeployerRaw is an auto generated low-level Go binding around an Ethereum contract.
+type DeployerRaw struct {
+	Contract *Deployer // Generic contract binding to access the raw methods on
+}
+
+// DeployerCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type DeployerCallerRaw struct {
+	Contract *DeployerCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// DeployerTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type DeployerTransactorRaw struct {
+	Contract *DeployerTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewDeployer creates a new instance of Deployer, bound to a specific deployed contract.
+func NewDeployer(address common.Address, backend bind.ContractBackend) (*Deployer, error) {
+	contract, err := bindDeployer(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Deployer{DeployerCaller: DeployerCaller{contract: contract}, DeployerTransactor: DeployerTransactor{contract: contract}, DeployerFilterer: DeployerFilterer{contract: contract}}, nil
+}
+
+// NewDeployerCaller creates a new read-only instance of Deployer, bound to a specific deployed contract.
+func NewDeployerCaller(address common.Address, caller bind.ContractCaller) (*DeployerCaller, error) {
+	contract, err := bindDeployer(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &DeployerCaller{contract: contract}, nil
+}
+
+// NewDeployerTransactor creates a new write-only instance of Deployer, bound to a specific deployed contract.
+func NewDeployerTransactor(address common.Address, transactor bind.ContractTransactor) (*DeployerTransactor, error) {
+	contract, err := bindDeployer(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &DeployerTransactor{contract: contract}, nil
+}
+
+// NewDeployerFilterer creates a new log filterer instance of Deployer, bound to a specific deployed contract.
+func NewDeployerFilterer(address common.Address, filterer bind.ContractFilterer) (*DeployerFilterer, error) {
+	contract, err := bindDeployer(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &DeployerFilterer{contract: contract}, nil
+}
+
+// bindDeployer binds a generic wrapper to an already deployed contract.
+func bindDeployer(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(DeployerABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Deployer *DeployerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Deployer.Contract.DeployerCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Deployer *DeployerRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Deployer.Contract.DeployerTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Deployer *DeployerRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Deployer.Contract.DeployerTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Deployer *DeployerCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Deployer.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Deployer *DeployerTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Deployer.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Deployer *DeployerTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Deployer.Contract.contract.Transact(opts, method, params...)
+}
+
+// Deploy is a paid mutator transaction binding the contract method 0x4af63f02.
+//
+// Solidity: function deploy(bytes code, bytes32 salt) returns(address addr)
+func (_Deployer *DeployerTransactor) Deploy(opts *bind.TransactOpts, code []byte, salt [32]byte) (*types.Transaction, error) {
+	return _Deployer.contract.Transact(opts, "deploy", code, salt)
+}
+
+// Deploy is a paid mutator transaction binding the contract method 0x4af63f02.
+//
+// Solidity: function deploy(bytes code, bytes32 salt) returns(address addr)
+func (_Deployer *DeployerSession) Deploy(code []byte, salt [32]byte) (*types.Transaction, error) {
+	return _Deployer.Contract.Deploy(&_Deployer.TransactOpts, code, salt)
+}
+
+// Deploy is a paid mutator transaction binding the contract method 0x4af63f02.
+//
+// Solidity: function deploy(bytes code, bytes32 salt) returns(address addr)
+func (_Deployer *DeployerTransactorSession) Deploy(code []byte, salt [32]byte) (*types.Transaction, error) {
+	return _Deployer.Contract.Deploy(&_Deployer.TransactOpts, code, salt)
+}
