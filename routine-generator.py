1857a2748651a3f8148d6e5d7716c34925cde394
Matt Garnett <matt.garnett@me.com>
2020-07-08 13:19:40 -0700

    tweak block dos params

--- routine-generator.py
+++ routine-generator.py
@@ -1,7 +1,7 @@
 import yaml
 from eth_utils import keccak, to_checksum_address, to_bytes
 
-LOOPS = 12000
+LOOPS = 12750
 
 DEPLOYER = "0xD2192C7F2EAEb1f05279c45D19828118e3D6f46C"
 AA_CODE = "0x6055600C60003960556000F33373ffffffffffffffffffffffffffffffffffffffff1460245736601f57005b600080fd5b60016020355b81900380602a576000358060005414604157600080fd5b600101600055604035604f57005b606035aa00"
@@ -130,7 +130,7 @@ def make_fixture(accounts, routines):
 
 def main():
     accounts = []
-    for i in range(0, 180):
+    for i in range(0, 188):
         accounts.append(Account(i, AA_CODE, 999999999))
 
     # make tx package
@@ -147,7 +147,7 @@ def main():
     # make block
     block_txs = []
     for a in accounts:
-        tx = AATransaction(a.addr, 0, 1, True, 1).as_tx(44000)
+        tx = AATransaction(a.addr, 0, 1, True, 1).as_tx(42500)
         block_txs.append(tx)
 
     for i in range(0, len(block_txs)):
