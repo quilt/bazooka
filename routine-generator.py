686678a5760dc6269a34f8f210f8959b66c19eee
Matt Garnett <matt.garnett@me.com>
2020-07-08 16:26:02 -0700

    improve block dos to 292 txs

--- routine-generator.py
+++ routine-generator.py
@@ -130,34 +130,43 @@ def make_fixture(accounts, routines):
 
 def main():
     accounts = []
-    for i in range(0, 188):
+    for i in range(0, 292):
         accounts.append(Account(i, AA_CODE, 999999999))
+
+    # make blocks to set nonce to non-zero value
+    txs = []
+    for a in accounts:
+        tx = AATransaction(a.addr, 0, 1, True, 1).as_tx(42500)
+        txs.append(tx)
+
+    init1 = make_routine(SEND_BLOCK, list(map(lambda x: x.as_obj(), txs[0:188])))
+    init2 = make_routine(SEND_BLOCK, list(map(lambda x: x.as_obj(), txs[188:])))
+
+    # make block to invalidate mempool
+    txs = []
+    for a in accounts:
+        tx = AATransaction(a.addr, 1, 1, True, 1).as_tx(27500)
+        txs.append(tx)
+
+    invalidator = make_routine(SEND_BLOCK, list(map(lambda x: x.as_obj(), txs)))
 
     # make tx package
     txs = []
     for a in accounts:
-        tx = AATransaction(a.addr, 0, LOOPS, True, 1).as_tx(400000)
+        tx = AATransaction(a.addr, 1, LOOPS, True, 1).as_tx(400000)
         txs.append(tx)
 
-    for i in range(0, len(txs)):
-        txs[i] = txs[i].as_obj()
+    tx_pkg = make_routine(SEND_TXS, list(map(lambda x: x.as_obj(), txs)))
 
-    txs_routine = make_routine(SEND_TXS, txs)
+    routines = [
+            init1,
+            init2,
+            make_routine(SLEEP, duration=2),
+            tx_pkg,
+            make_routine(SLEEP, duration=2),
+            invalidator
+    ]
 
-    # make block
-    block_txs = []
-    for a in accounts:
-        tx = AATransaction(a.addr, 0, 1, True, 1).as_tx(42500)
-        block_txs.append(tx)
-
-    for i in range(0, len(block_txs)):
-        block_txs[i] = block_txs[i].as_obj()
-
-    block_routine = make_routine(SEND_BLOCK, block_txs)
-
-
-    routines = [txs_routine, make_routine(SLEEP, duration=2), block_routine]
-    #  routines = [block_routine]
     print(yaml.dump(make_fixture(accounts, routines)))
 
 
