e91ead5f5db88fb122baf9fcdab64e3cda2fb8c8
lightclient@protonmail.com <lightclient@protonmail.com>
2020-09-01 14:26:42 -0600

    update routines for 15k intrinsic gas

--- routines/generator/block_dos.py
+++ routines/generator/block_dos.py
@@ -2,8 +2,9 @@ from account import ContractAccount
 from transaction import AATransaction, Transaction
 from helpers import make_fixture, make_routine, SEND_BLOCK, SEND_TXS, SLEEP
 from constants import AA_CODE, BLOCK_LIMIT, DEPLOYER
+import time
 
-BASE_GAS = 27470
+BASE_GAS = 21470
 LOOP_GAS = 26
 
 def make(gas_limit, block_limit=BLOCK_LIMIT):
@@ -16,17 +17,18 @@ def make(gas_limit, block_limit=BLOCK_LIMIT):
 
     # make blocks to set nonce to non-zero value
     txs = []
+    init_blocks = []
     for a in accounts:
-        tx = AATransaction(a.addr, 0, 1, True, 1).as_tx(42500)
+        tx = AATransaction(a.addr, 0, 1, True, 1).as_tx(36500)
         txs.append(tx)
 
-    init1 = make_routine(SEND_BLOCK, list(map(lambda x: x.as_obj(), txs[0:188])))
-    init2 = make_routine(SEND_BLOCK, list(map(lambda x: x.as_obj(), txs[188:])))
+    for i in range(0, len(txs), 200):
+        init_blocks.append(make_routine(SEND_BLOCK, list(map(lambda x: x.as_obj(), txs[i:i+200]))))
 
     # make block to invalidate mempool
     txs = []
     for a in accounts:
-        tx = AATransaction(a.addr, 1, 1, True, 1).as_tx(27500)
+        tx = AATransaction(a.addr, 1, 1, True, 1).as_tx(21500)
         txs.append(tx)
 
     invalidator = make_routine(SEND_BLOCK, list(map(lambda x: x.as_obj(), txs)))
@@ -39,13 +41,10 @@ def make(gas_limit, block_limit=BLOCK_LIMIT):
 
     tx_pkg = make_routine(SEND_TXS, list(map(lambda x: x.as_obj(), txs)))
 
-    routines = [
-            init1,
-            init2,
-            make_routine(SLEEP, duration=2),
-            tx_pkg,
-            make_routine(SLEEP, duration=2),
-            invalidator
-    ]
+    routines = init_blocks
+    routines.append(make_routine(SLEEP, duration=2))
+    routines.append(tx_pkg)
+    routines.append(make_routine(SLEEP, duration=2))
+    routines.append(invalidator)
 
     return make_fixture(accounts, routines)
